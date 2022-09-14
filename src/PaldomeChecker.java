c82555e8eb1891e73b474f6c6a27c2025a8b9115
Creeeeed <89224604+Creeeeed@users.noreply.github.com>
2022-09-14 09:15:45 -0600

    Initial commit

---
+++ src/PaldomeChecker.java
@@ -0,0 +1,26 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class PaldomeChecker {
+    public static boolean checker(String canidate ){
+        return true;
+// make sure to make a
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+
+        FileReader reader = new FileReader( "palidome.txt");
+        try{
+            Scanner scan = new Scanner(reader);
+            while(scan.hasNext()){
+                String value = scan.nextLine();
+                checker(value);
+            }
+
+        }
+        catch (Exception e){
+            e.getMessage();
+        }
+
+    }
+}
